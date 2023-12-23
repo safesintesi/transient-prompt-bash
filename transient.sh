7ff9cab8aab25985f7d2d8f5fcef8761ebe0a698
safesintesi <edoardobortolozzo@gmail.com>
2023-12-23 18:19:04 +0100

    cleaned some code and set default PS1

--- transient.sh
+++ transient.sh
@@ -3,7 +3,7 @@ function lastcommand {
     history | tail -1 | cut -c 8-
 }
 function deleteprompt {
-    for i in $(seq $(($(echo -ne "$PS1" | wc -l) + 1)));
+    for i in $(fgrep -o '\n' <<< $PS1'\n')
     do
         tput cuu 1
         tput el
@@ -13,11 +13,9 @@ function deleteprompt {
 DIR_PROMPT="\[$(tput setaf 6)\]\w"
 GIT_PROMPT="\[$(tput setaf 5)\]$(git --version &> /dev/null && echo -n '$(__git_ps1)')"
 
-# uncomment this to reset the prompt or edit it to use a different one
+# comment this to reset the prompt or edit it to use a different one
 PS1=$DIR_PROMPT" "$GIT_PROMPT"\n\[$(tput setaf 2)\]\u@\h\[$(tput sgr0)\] \$ "
 
-# uncomment this to reset the prompt or edit it to use a different one
-# PS1='\[$(tput setaf 6)\]\w\n\[$(tput setaf 2)\]\u@\h\[$(tput sgr0)\] \$ '
 PS0='\[$(deleteprompt)\]\$ $(lastcommand)\n\[${PS1:0:$((EXPS0=1,0))}\]'
 PROMPT_COMMAND='[ "$EXPS0" = 0 ] && deleteprompt && echo -e "\$" || EXPS0=0'
 
